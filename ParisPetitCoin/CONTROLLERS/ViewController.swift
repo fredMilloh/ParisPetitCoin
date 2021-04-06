7e455c22fd455c691653efa01974ab1a8a5717aa
fred milloh <fred.milloh@hotmail.com>
2021-04-06 19:26:32 +0200

    add TableViewController, callback

--- ParisPetitCoin/CONTROLLERS/ViewController.swift
+++ ParisPetitCoin/CONTROLLERS/ViewController.swift
@@ -9,14 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //https://www.data.gouv.fr/fr/datasets/r/9cf8fab8-997c-4814-9600-8c17bc3de7e0
-
     override func viewDidLoad() {
         super.viewDidLoad()
-    let service = Service(baseUrl: "https://www.data.gouv.fr/fr/datasets/r/9cf8fab8-997c-4814-9600-8c17bc3de7e0")
-        service.getDataSet()
+        //let service = Service()
+        //service.getDataSet()
     }
 
-
+    @IBAction func listButton(_ sender: UIButton) {
+    }
+    
 }
 
