f388e49bfb588d2a36bfa51d71bcdfa0708a465f
fred milloh <fred.milloh@hotmail.com>
2021-04-09 09:40:57 +0200

    add user location

--- ParisPetitCoin/CONTROLLERS/ViewController.swift
+++ ParisPetitCoin/CONTROLLERS/ViewController.swift
@@ -7,19 +7,47 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
     
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupManager()
         //let service = Service()
         //service.getDataSet()
+    }
+    
+    func setupManager() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
     }
 
     @IBAction func listButton(_ sender: UIButton) {
     }
     
 }
-
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.startUpdatingLocation()
+            configureRegion(location)
+        }
+    }
+    
+    func configureRegion(_ location: CLLocation) {
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: span)
+        
+        mapView.setRegion(region, animated: true)
+        mapView.showsUserLocation = true
+    }
+    
+}
