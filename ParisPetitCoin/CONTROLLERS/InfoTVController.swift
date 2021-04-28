b2131e6f657f235cad6d7545778474809e93680a
fred milloh <fred.milloh@hotmail.com>
2021-04-28 16:38:52 +0200

    add webView and progressView and setups

--- ParisPetitCoin/CONTROLLERS/InfoTVController.swift
+++ ParisPetitCoin/CONTROLLERS/InfoTVController.swift
@@ -6,13 +6,21 @@
 //
 
 import UIKit
+import WebKit
 
 class InfoTVController: UITableViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    var progressView: UIProgressView!
     
     var selectedToilette: ToiletteAnnotation?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupProgressView()
+        setupWebView()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -20,28 +28,60 @@ class InfoTVController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?,
+                               change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+
+    func setupProgressView() {
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        webView.addSubview(progressView)
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        
+        progressView.leadingAnchor.constraint(equalTo: webView.leadingAnchor).isActive = true
+        progressView.trailingAnchor.constraint(equalTo: webView.trailingAnchor).isActive = true
+        progressView.topAnchor.constraint(equalTo: webView.safeAreaLayoutGuide.topAnchor).isActive = true
+        
+        let keyPath = #keyPath(WKWebView.estimatedProgress)
+        webView.addObserver(self, forKeyPath: keyPath, options: .new, context: nil)
+    }
+    
+    func setupWebView() {
+        let url = selectedToilette?.url
+        if url == "" {
+            webView.backgroundColor = .blue
+        } else {
+        let urlToilette = URL(string: url!)
+        webView.load(URLRequest(url: urlToilette!))
+        }
+    }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "InfoCell", for: indexPath)
 
-        // Configure the cell...
+        cell.textLabel?.text = selectedToilette?.title
+        cell.detailTextLabel?.text = selectedToilette?.adresse
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
